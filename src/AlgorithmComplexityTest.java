5de26171ea8633086d8b3b7cb8b789938d87bac4
Hotrung2311 <trungho2311@gmail.com>
2020-04-24 09:44:27 +0700

    Đếm ký tự xuất hiện nhiều nhất trong chuỗi

---
+++ src/AlgorithmComplexityTest.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class AlgorithmComplexityTest {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a string:");
+        String inputString = scanner.nextLine();
+
+        int[] frequentChar = new int[255];
+        for (int i = 0; i < inputString.length(); i++) {
+            int ascii = (int) inputString.charAt(i);
+            frequentChar[ascii]++;
+        }
+        int max = 0;
+        char character = (char) 255; /* empty character */
+        for (int j = 0; j < 255; j++) {
+            if (frequentChar[j] > max) {
+                max = frequentChar[j];
+                character = (char) j;
+            }
+        }
+        System.out.println("Chữ cái xuất hiện nhiều nhất là '" + character + "' với tần suất là " + max + " lần.");
+    }
+}
